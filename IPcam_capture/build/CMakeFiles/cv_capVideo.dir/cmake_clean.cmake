856f9c7e1e1f77b54d8f9357d4348b5cefe81dc7
Bhavin Darji <bhavindarji@users.noreply.github.com>
2018-05-29 10:54:04 +0530

    DCS-2100 IP cam C++ example

---
+++ IPcam_capture/build/CMakeFiles/cv_capVideo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cv_capVideo.dir/capVideo.cpp.o"
+  "cv_capVideo.pdb"
+  "cv_capVideo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cv_capVideo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
